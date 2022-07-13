f3350c7c9e1194bade4ba2b3b49a660ec41ba594
Alyson Morais <alysonmorais35@hotmail.com>
2022-07-13 16:12:00 -0300

    Implement Traveler logo and fps control

---
+++ Assets/_Game/Scripts/GameManager/FpsControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsControl : MonoBehaviour
+{
+    public float timer, refresh, avgFramerate;
+    public string display = "{0} FPS";
+    public Text m_Text;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 1;
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+    {
+#if DEVELOPMENT_BUILD
+        float timelapse = Time.smoothDeltaTime;
+        timer = timer <= 0 ? refresh : timer -= timelapse;
+
+        if (timer <= 0) avgFramerate = (int)(1f / timelapse);
+        m_Text.text = string.Format(display, avgFramerate.ToString());
+#endif
+    }
+}
